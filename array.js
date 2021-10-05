2e10ef58c7d34deaadc4984d4adb53add34ac115
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-10-05 17:09:15 +0530

    basic layout made

---
+++ array.js
@@ -0,0 +1,15 @@
+// let a = [2, 3, 4, 2, 1, 2, 4, 3, 4, 2];
+
+// let size = 3;
+// let arr = [];
+// let c = 0;
+// for (let i = 0; i<a.length; i += 1) {
+//   let arr2 = [];
+//   for (let j = 0; j < size; j += 1) {
+//     arr2.push(a[c]);
+//     c++;
+//   }
+//   arr.push(arr2);
+//   i=c;
+// }
+// console.log(arr);
